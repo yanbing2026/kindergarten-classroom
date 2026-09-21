1950cc4b863385463e49aa8dd6a5421f28e62a06
yanbing2026 <yanbing2004@gmail.com>
2026-09-21 12:07:33 -0400

    Cache Blipola voice module

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'kc-shell-v4';
+const CACHE_NAME = 'kc-shell-v5';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
   './js/blipola-runtime.js',
+  './js/blipola-voice.js',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/apple-touch-icon.png',
